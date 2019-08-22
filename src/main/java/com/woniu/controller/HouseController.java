9b90941f37b16400fea98c81144443ca8a69e951
liu <1312275689@qq.com>
2019-08-22 14:59:37 +0800

    二次提交

--- src/main/java/com/woniu/controller/HouseController.java
+++ src/main/java/com/woniu/controller/HouseController.java
@@ -14,7 +14,7 @@ import com.woniu.service.IhouseService;
 @RestController
 @RequestMapping("admin/houses")
 public class HouseController {
-
+ 
 	@Resource
 	private IhouseService houseServiceImpl;
 	
